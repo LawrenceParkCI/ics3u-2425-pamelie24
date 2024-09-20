66a9ad5e0edf336f0f9774c717e295a361467da5
350254384 <350254384@MXL1502T7Y.tdsb.on.ca>
2024-09-20 11:38:19 -0400

    This Code is about me, it describes different things I like.

---
+++ ICS3U-PameliE/src/unit1/AboutMe.java
@@ -0,0 +1,20 @@
+package unit1;
+/**
+ * Description: This program describes about me, It covers my name, school, teacher, and my grade
+ *Date: Sept. 20, 2024
+ *@author Ethan Pameli 
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello my nams is Ethan Joseph Pameli");
+		System.out.println("I am a grade 11 student");
+		System.out.println("My Computer teacher is Ms. Kemp");
+		System.out.println("I am a student at LPCI");
+		System.out.println("I like to play the sport of HOCKEY W Hockey");
+		System.out.println("I am very fond of this class");
+
+	}
+
+}
