a25329c12ebd7a2799bb805547e7f5eff5c3db0b
350254384 <350254384@MXL1502T7Y.tdsb.on.ca>
2024-09-23 09:43:47 -0400

    I added lines and some random words that dont work

--- ICS3U-PameliE/src/unit1/AboutMe.java
+++ ICS3U-PameliE/src/unit1/AboutMe.java
@@ -8,12 +8,15 @@ public class AboutMe {
 
 	public static void main(String[] args) {
 
+		System.out.println("About me");
+		System.out.println("---------");
 		System.out.println("Hello my nams is Ethan Joseph Pameli");
 		System.out.println("I am a grade 11 student");
 		System.out.println("My Computer teacher is Ms. Kemp");
 		System.out.println("I am a student at LPCI");
-		System.out.println("I like to play the sport of HOCKEY W Hockey");
-		System.out.println("I am very fond of this class");
+		System.out.println("I Love LPCI's Hockey team, Go Panthers");
+		System.out.println("--------------------------------------");
+		System.out.format("Date: %-15sTime: %8s", "September 23rd");
 
 	}
 
