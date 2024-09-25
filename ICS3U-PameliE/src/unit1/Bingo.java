d454722ea309338634ffbf780a63414508950903
350254384 <350254384@MXL1502T7Y.tdsb.on.ca>
2024-09-25 10:26:57 -0400

    This is the start of my bingo card, I have the rules to the game.

---
+++ ICS3U-PameliE/src/unit1/Bingo.java
@@ -0,0 +1,20 @@
+package unit1;
+
+public class Bingo {
+
+	public static void main(String[] args) {
+		// The Bingo Steps 
+		System.out.println("THE BIG BINGO CARD!!!!!");
+		System.out.println("1. The Caller randomly pulls out a numbered bingo ball");
+		System.out.println("2. The number is placed on the bingo board and called out");
+		System.out.println("3. Players look for the called outnumber on their bingo card");
+		System.out.println("4. If the number is located, it is marked off on the players card");
+		System.out.println("5. Repeat steps 1-4 until someone scores a BINGO");
+		System.out.println("6. The winning player must call out BINGO to make it official");
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("\n");
+		
+
+	}
+
+}
