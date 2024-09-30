a3fa5d4bc15d8d42308a61a35183e9ffbaa2cccf
350254384 <350254384@MXL1502T7Y.tdsb.on.ca>
2024-09-30 12:01:36 -0400

    Completed ASCII art images and name

--- ICS3U-PameliE/src/unit1/Asciiart.java
+++ ICS3U-PameliE/src/unit1/Asciiart.java
@@ -25,7 +25,50 @@ public class Asciiart {
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++x$&&$Xxx+;;;;;;;;;::::::;;;;;;;;;+xX$$$&&&&&&$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$$");
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++x&&&$x+++;;;:::::::::::::::;;;;;;;;;+x$&&&&&&&&XxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$$");
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++X&&$Xx++;;;;:::::::::::::::;;;;;;;;;++x$&&&&&&&$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$$");
-		System.out.println("");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++x&&&$xxx+;;;;;:::::::::::::::;;;;;;;;++x$$&&&&&&$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$$$");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++x&&&Xxx++;;;;;:::::::::::::::;;;;;;;;++X$&&&&&&&$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$$");
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++&&&Xxx++;;;;:::::::::::::::::;;;;;;;++X$&&&&&&&$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$$");
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++x&&$xx+++;;;;;::::::::::::::::;;;;;;++xx$&&&&X+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++X&$Xx+++;;;:::::::::::::::::::;;;;;++xxX&&&X+Xx+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$");
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xX&Xxx+;;;::::::::::...::::::;;;;;;+++xx$&$xx;++xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++x&$xx+;;;;::::::::::::;;+xXXXxxxxXXxxxxx$$xx;;+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++x$$$$$$$$Xx++;;:::;;+xX$$$$XXXXXXXx++++xX&X+;;;+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++X$$$XXX$$&&$Xx;;::;xXXXx+$$XxXXx++;;;+xx$X;:;+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xX$$$&$X$$$+xXx;::;+;;;+;;;++x+;;;;;;++xXX+;;+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xXXXX$X+;;;;;+x;::;;;;;;+++++;::::;;;++xXX+;+X$$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++Xx++xxxxx++++x;::;;;;;;:;;;::::::;;;+xxXX++X$$$$XxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xX++;++;;+++++;::;;;;;;:::::::::;;;++xxX$&$$$$$$$$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xXx++;;;;;;++;::::;;;;;::::::::;;;++xxxX$$$$$$$$$$$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xxxX$Xx+;;;;;;++;::::;;;;;;:::::::;;+++xxxX$$$$$$$$$$$$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xxxx$&&$Xx+;;;;;xXx+++xXXx+x;::::;;;;;+++++xxX$$$$$$$$$$$$XxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xxx$$&&&$Xx++;;;x$$$$Xx++++;:::::;;;;;+++++xxX$$$X$$$$$$$$$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++xxx$$&&&&$$Xx++;;++xxx+;;;;;;::;;;;;;;+++++++xX$$XXX$$$$$$$$$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++xxx+++xxX$$$$&&&&$$Xxx+++++++;;;:;;;;;;;;;;;;++++++xx$$XXXXX$$$$$$$$$XxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++xxxxxx+xxX$$$$$$&&&&$$XXxx++xxx+++xxxxxxxx++;;++++++xx$$$XXXXXX$$$$$$$$$XxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++xxxxxxxxxX$$$$$$$$$$&&$$XxXxxXXXxx+++;;;+x++++++++++xX$&$$$XXXXXX$$$$$$$$$XxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++xxxxx$$$$$$$$$$$$$&&$$$XxxXXx+++++xxXx+++++++++xxX&&$$$XXXXXXXX$$$$$$$$$xxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++xxX$X$$$$$$$$$$X$$&&&$$XxxXX$$$Xxx+;;;;;;+++xxX$&$$$$$$$XXXXXXX$$$$$$$$$xxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++xX$X$$$$$$$$$$XXX$$&&&$$xxxx++;;:::;;;;;;+xxX$&$XX$$$$$$XXXXXXXXX$$$$$X$XxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++++x+++xX$X$$$$$$$$$$$XXXX$&&&&&$Xxx+++;;;;;++++xxX$&$XxxX$$$$$XXXXXXXXXXXXXX$$X$XxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++xxxxxxX$$$$$$$$$$$$$XXXXX$$&&&&&&$XXXxxxxxxxXXX$&&$$Xx+x$$$$$$XXXXXXXXXXXXXX$$X$$XxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++++++++++++xxX$$$$$$$$$$$$$$XXXXX$$$&&&&&&&&&&&&&&&&&&&&&$Xx+++X$$$$$XXXXXXXXXXXXXXXX$$XX$XXxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++++++xxxXX$$$$$$$$$$$$$$$XXXXXX$$&&&&&&&&&&&&&&&&&&$$XXx++++$$$$$$XXXXXX$XXXXXXXXX$$$X$$$$$$$XXxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++++xxX$$$$$$$$$$$$$$$$$$$$XXXXXXX$&&&&&&&$$$$$$$$$$$XXXxx++++X$$$$$$XXXXXXXXXXXXXXXXX$$XX$$$$$$$$$$$XXxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++++++x$$$$$$$$$$$$$$$$$$$$$$$$XXXXXXX$&&&&&&&&&&&&&&$$$$$$$$$&&&&$$$$$$XXXXXXXXXXX$$X$$X$$$$X$$$$$$$$$$$$$$$$$XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++++X$$$$$$$$$$$$$$$$$$$$$$$$$XXXXXXX$&&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$$$X$$XXXXXXXX$$$$$$$$$XX$$$$$$$$$$$$$$$$$$$$$XXXXXXXXXXXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++x$$$$$$$$$$$$$$$$$$$$$$$$$$$XXXXXX$$&&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$$$XXXXXXXXXXXXXXX$$$$$$XX$$$$$$$$$$$$$$$$$$$$$$$$XXXXXXXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++++X$$$$$$$$$&&&&&&&&&&&&$$$$$$$XXXXX$&&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$$$$$$XX$XX$$$$$$$$$$$$$$$X$$$$$$$$$$$$$$$$$$$$$$$$$$XXXXXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++++X$$$$$$$$$$$&&&&&&&&&&&&&&&&$$XXXXX$&&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$$$$$$$$XXXXXXXXXXXXXX$$$$$$$$$$$$$$$$$$$$$$$$$$$&$&&XXXXXXXXXXXXXXXXXXXXXX$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++xX$$$$$$$$$$$XX$$$$&&$$$$$$$$X$XXXX$$&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$$$$$$$$$$$$&&&&&&&&&&&&&&&&&&$$$$$$$$$$$$$$$$$$$$&&&&$XXXXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++x$X$$$$$$$$XXXXXX$$$$$$$$$$$$$XXXXX$$&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$$$&&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$$$$$$$$$$$$$$$$$&&&&XXXXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++++x$X$$$$$$$XXXX$$$$$$$$$$$$$$$$$XXXX$&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$$$$X$$$$$$$$$$$$$$$$$$$&&&&$$$$$$$$$$$$$$$$$$$$$$$&&&&&$XXXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++xX$X$$$$$$XXX$$$$$$$$$$$$$$$$$$$$XX$$&&&&&&&&&&&&&&&&&&&&&&&&&&$$$$$XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$$$$$$$$$$$$$$$$$&&&&&&&$XXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++x$$X$$$$$XXXX$$$$$$$$$$$$$$$$$$$$XX$$&&&&&&&&&&&&&&&&&&&&&&&&&$$$$XXXXXXXXXXXXXXX$$$$$$$$$$$$$$$$XX$$$$$XX$$$X$$$$$$$&&&&&&&$XXXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++++X$$X$$$$$XXXX$$$$$$$$$$$$$$$$$$$XX$$$&&&&&&&&&&&&&&&&&&&&&&&&&$$$$XXXXXXXXXXXXXXXXXXXXXXXXXXXXX$XX$$$$$$$X$$$$$$$$$$$&&$$$&&&$XXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++x$$$$X$$$XXXX$$$$$$$$$$$$$$$$$$$$XX$$&&&&&&&&&&&&&&&&&&&&&&&&&$$$XXXXXXXXXXXXXXXXXX$$$$$$$$$$XX$$XX$$X$$$$$$$$$$$$$$$$&&$$$&&&$XXXXXXXXXXXXXXXXXX$$\r\n"
+				         + "++++++++++++++++++++++++++++++++++++$$$$$X$$$XXXX$$$$$$$$$$$$$$$$$$$$XX$$&&&&&&&&&&&&&&&&&&&&&&&&&$$XXXXXXXXXXXXXXXXXXXXXXXXXXX$$XX$$X$$$X$$$$$$$$$$X$$$$$&$$$&&&&$XXXXXXXXXXXXXXXXX$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++x$$$$$X$$XXXX$$$$$$$$$$$$$$$$$$$$XXX$$&&&&&&&&&&&&&&&&&&&&&&&&&$$XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$X$$$$X$$$$$$$$$$$$$$$$$$$$$$&&&$XXXXXXXXXXXXXXXX$$$\r\n"
+				         + "+++++++++++++++++++++++++++++++++++X$$$$$$$$XXX$$$$$$$$$$$$$$$$$$$$$XX$$$&&&&&&&&&&&&&&&&&&&&&&&&$$XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX$$XX$&$XX$$$$$$$$$$$$$$$$$$$$&&&&&&$XXXXXXXXXXXXXXX$$$");
 	}
 	
 
