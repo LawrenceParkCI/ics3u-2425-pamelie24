43e106923a581ab4ba7fb504f7fb4bcc60d31744
350254384 <350254384@MXL1502T7Y.tdsb.on.ca>
2024-10-02 11:38:36 -0400

    w

--- ICS3U-PameliE/src/unit1/RectangleArea.java
+++ ICS3U-PameliE/src/unit1/RectangleArea.java
@@ -1,9 +1,24 @@
 package unit1;
 
+import java.util.Scanner;
+
 public class RectangleArea {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner (System.in);
+		//declare variables
+		int length, width;
+		
+		//get the user input 
+		System.out.println("AREA PROGRAM");
+		System.out.print("Type in the length of the rectangel and <Enter>:");
+		length = sc.nextInt();
+		
+		System.out.print("Type in the width of the rectangle and <Enter>:");
+		width = sc.nextInt();
+
 
 	}
 
